a4404cef6e5feb070a25860c7071a33b78496d60
GourSahil <248735070+GourSahil@users.noreply.github.com>
2026-05-23 09:19:26 +0530

    zeroing .bss

--- kernel/boot.asm
+++ kernel/boot.asm
@@ -2,8 +2,8 @@ section .multiboot
 align 8
 
 mb_header_start:
-    dd 0xe85250d6                ; magic
-    dd 0                         ; architecture
+    dd 0xe85250d6
+    dd 0
     dd mb_header_end - mb_header_start
     dd -(0xe85250d6 + 0 + (mb_header_end - mb_header_start))
 
@@ -13,12 +13,37 @@ mb_header_start:
 
 mb_header_end:
 
+section .bss
+align 16
+
+stack_bottom:
+    resb 16384
+stack_top:
+
 section .text
 global _start
+
 extern kernel_main
+extern __bss_start
+extern __bss_end
 
 _start:
     cli
+
+    ; setup 64-bit stack
+    mov rsp, stack_top
+
+    ; zero .bss section
+    mov rdi, __bss_start
+    mov rcx, __bss_end
+
+    sub rcx, rdi
+
+    xor rax, rax
+
+    rep stosb
+
+    ; call kernel
     call kernel_main
 
 .hang:
